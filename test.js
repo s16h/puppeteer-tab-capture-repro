014defdf3118a9b832287de11d87dec595902046
Shahriar Tajbakhsh <sh.tajbakhsh@gmail.com>
2018-12-14 11:54:48 +0000

    Add Puppeteer script

---
+++ test.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const pathToExtension = require('path').join(__dirname, 'test-extension');
+    const extensionId = 'ngjfomcnndimpojnjmahjjglkpogpbpl';
+
+    const browser = await puppeteer.launch({
+        args: [
+            `--whitelisted-extension-id=${extensionId}`,
+            '--use-fake-ui-for-media-stream',
+            `--disable-extensions-except=${pathToExtension}`,
+            `--load-extension=${pathToExtension}`,
+        ],
+        headless: false,
+    });
+    const page = await browser.newPage();
+
+    await page.goto('https://google.com', { waitUntil: 'networkidle2' });
+
+    const targets = await browser.targets();
+    const backgroundPageTarget = targets.find(target => target.type() === 'background_page' && target.url().startsWith(`chrome-extension://${extensionId}/`));
+    const backgroundPage = await backgroundPageTarget.page();
+
+    backgroundPage.on('console', msg => {
+        for (let i = 0; i < msg.args().length; i++) {
+            console.log(`${i}: ${msg.args()[i]}`);
+        }
+    });
+
+    await backgroundPage.evaluate(() => {
+        startRecording();
+        return Promise.resolve(42);
+    });
+
+    await page.waitFor(120 * 1000);
+    await browser.close();
+})();
